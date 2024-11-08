f8639a2beb40460a194cf47efc4f1e634f6a3d51
Маргарита Комарова <Tyuiu.komarovaMV@outlook.com>
2024-11-08 22:49:36 +0500

    Task2

--- Tyuiu.KomarovaMV.Sprint5.Task2.V9.Lib/DataService.cs
+++ Tyuiu.KomarovaMV.Sprint5.Task2.V9.Lib/DataService.cs
@@ -8,30 +8,29 @@ namespace Tyuiu.KomarovaMV.Sprint5.Task2.V9.Lib
         {
             string path = Path.GetTempFileName();
             FileInfo fileInfo = new FileInfo(path);
-            if (fileInfo.Exists) {File.Delete(path); }
+            if (fileInfo.Exists) { File.Delete(path); }
             int rows = matrix.GetUpperBound(0) + 1;
-            int cols=matrix.Length/rows;
-            string str = "";
+            int cols = matrix.Length / rows;
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
                     if (matrix[i, j] % 2 != 0) { matrix[i, j] = 0; }
                 }
-                for (int x = 0; x < rows; x++)
+            }
+            string str = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
                 {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        if (j != cols - 1) { str += matrix[i, j] + ";"; }
-                        else { str += matrix[i, j]; }
+                    if (j != cols - 1) { str += matrix[i, j] + ";"; }
+                    else { str += matrix[i, j]; }
 
-                    }
-                    if (x != rows - 1) { File.AppendAllText(path, str + Environment.NewLine); }
-                    else { File.AppendAllText(path, str); }
                 }
+                if (i != rows - 1) { File.AppendAllText(path, str + Environment.NewLine); }
+                else { File.AppendAllText(path, str); }
                 str = "";
             }
-            
             return path;
         }
     }
